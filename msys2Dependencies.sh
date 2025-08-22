7a4b5690bd2c2c34cddd0e2f5c828dbb65c7040d
Sahil Gautam <sahil.gautam@collabora.com>
2025-08-22 09:37:07 +0530

    install clangd in clang64 env not through vscode extension
    + add some notes related to setup (git and clangd)
    + change clangd path in settings.json to point to clang64's clangd
    + no need to enable gpg signing

--- msys2Dependencies.sh
+++ msys2Dependencies.sh
@@ -3,4 +3,5 @@
 pacman -S clang64/mingw-w64-clang-x86_64-gdb \
           clang64/mingw-w64-clang-x86_64-cmake \
           clang64/mingw-w64-clang-x86_64-ninja \
-          clang64/mingw-w64-clang-x86_64-clang
+          clang64/mingw-w64-clang-x86_64-clang \
+          clang64/mingw-w64-clang-x86_64-clang-tools-extra
