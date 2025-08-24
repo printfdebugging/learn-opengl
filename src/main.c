b9560e9e74292362897f7fbeaf3eccdb0ffb69c3
Sahil Gautam <sahil.gautam@collabora.com>
2025-08-24 10:27:20 +0530

    [0001] create an empty window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const char* WINDOW_TITLE = "LearnOpenGL";
+
+void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main() {
-    printf("debugging\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "failed to create glfw winodw\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        fprintf(stderr, "failed to initialize glad\n");
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebufferResizeCallback(GLFWwindow* /* window */, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_TRUE) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 }
