e34f9ced51effb25e46d461660bd9c4ba5c41478
Sahil Gautam <sahil.gautam@collabora.com>
2025-09-10 00:32:22 +0530

    delete all abstractions and back to main.c and hello-window

--- src/main.c
+++ src/main.c
@@ -1,83 +1,68 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include <stdio.h>
 #include <stdlib.h>
-#include "shader.h"
-#include "window.h"
-#include "mesh.h"
-#include "renderer.h"
 
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-const char *WINDOW_TITLE = "LearnOpenGL";
+#define ERROR(message, ...)                                \
+    fprintf(stderr, "%s:%i: error: ", __FILE__, __LINE__); \
+    fprintf(stderr, message, ##__VA_ARGS__)
 
-/* clang-format off */
-GLfloat right_rect[] = {
-    0.75f, 0.75f, 0.0f,
-    0.75f, 0.25f, 0.0f,
-    0.25f, 0.25f, 0.0f,
-    0.25f, 0.75f, 0.0f,
-};
+static void framebuffer_resize_callback(GLFWwindow* window, int width, int height) {
+    (void) window;
+    glViewport(0, 0, width, height);
+}
 
-GLfloat left_rect[] = {
-    -0.25f, 0.75f, 0.0f,
-    -0.75f, 0.75f, 0.0f,
-    -0.75f, 0.25f, 0.0f,
-    -0.25f, 0.25f, 0.0f,
-};
-
-GLuint right_rect_indices[] = {
-    0, 1, 2,
-    0, 2, 3,
-};
-
-GLuint left_rect_indices[] = {
-    0, 1, 2,
-    0, 2, 3,
-};
-/* clang-format on */
+static void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
 
 int main() {
-    GLFWwindow *window = window_create(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    if (!glfwInit()) {
+        ERROR("failed to initialize glfw");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
     if (!window) {
+        ERROR("failed to create glfw winodw\n");
+        glfwTerminate();
         return EXIT_FAILURE;
     }
 
-    struct mesh right_rect_mesh = mesh_create();
-    if (!mesh_load_vertices(&right_rect_mesh, right_rect, sizeof(right_rect), 3 * sizeof(float), GL_STATIC_DRAW)) {
-        return EXIT_FAILURE;
-    }
-    if (!mesh_load_indices(&right_rect_mesh, right_rect_indices, sizeof(right_rect_indices), GL_STATIC_DRAW)) {
-        return EXIT_FAILURE;
-    }
-    if (!mesh_load_shader_program(&right_rect_mesh, "shaders/shader.vert", "shaders/shader.frag")) {
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        ERROR("failed to initialize glad\n");
+        glfwTerminate();
         return EXIT_FAILURE;
     }
 
-    struct mesh left_rect_mesh = mesh_create();
-    if (!mesh_load_vertices(&left_rect_mesh, left_rect, sizeof(left_rect), 3 * sizeof(float), GL_STATIC_DRAW)) {
-        return EXIT_FAILURE;
-    }
-    if (!mesh_load_indices(&left_rect_mesh, left_rect_indices, sizeof(left_rect_indices), GL_STATIC_DRAW)) {
-        return EXIT_FAILURE;
-    }
-    if (!mesh_load_shader_program(&left_rect_mesh, "shaders/shader.vert", "shaders/shader.frag")) {
-        return EXIT_FAILURE;
-    }
-
-    renderer_init();
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     while (!glfwWindowShouldClose(window)) {
-        window_process_input(window);
+        /* event */
+        glfwPollEvents();
+        process_input(window);
+        /* event */
 
-        renderer_prepare();
+        /* renderer */
+        glClear(GL_COLOR_BUFFER_BIT);
+        /* renderer */
 
-        renderer_render(right_rect_mesh, INDICES_WIREFRAME);
-        renderer_render(left_rect_mesh, INDICES_WIREFRAME);
-
-        window_refresh(window);
+        glfwSwapBuffers(window);
     }
 
-    mesh_destroy(right_rect_mesh);
-    mesh_destroy(left_rect_mesh);
-    window_destroy(window);
-
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
