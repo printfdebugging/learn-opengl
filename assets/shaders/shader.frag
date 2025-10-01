ea3a5dac0f7f6bbd5f77c70e812fd9ff613a5efb
Sahil Gautam <printfdebugging@gmail.com>
2025-10-01 22:23:48 +0530

    textures done, just some learnopengl exercises left

--- assets/shaders/shader.frag
+++ assets/shaders/shader.frag
@@ -1,8 +1,29 @@
 #version 330 core
 
-out vec4 fragment_color;
-in vec4 pos_to_frag_shader;
+in  vec4 color;
+in  vec2 uv;
+
+/*
+    variables which are not consumed by the shaders 
+    but are passed on to opengl have an '_' at the
+    end.
+*/
+out vec4 color_;
+
+
+/*
+    uniforms are all upper case. sampler2D are just integers
+    under the hood, handles to shader objects. that's why we set
+    them with glUniform1i in the c code.
+*/
+uniform sampler2D TEXTURE0;
+uniform sampler2D TEXTURE1;
+
 
 void main() {
-    fragment_color = pos_to_frag_shader;
+    color_ = mix(
+        texture(TEXTURE0, uv),
+        texture(TEXTURE1, uv),
+        0.2
+    );
 }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "stb_image.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -131,7 +132,9 @@ int main() {
     glAttachShader(shader_program, fragment_shader);
 
     /* bind variables to certain slot locations in the vao */
-    glBindAttribLocation(shader_program, 0, "vertex_position");
+    glBindAttribLocation(shader_program, 0, "_position");
+    glBindAttribLocation(shader_program, 1, "_color");
+    glBindAttribLocation(shader_program, 2, "_uv");
     glLinkProgram(shader_program);
 
     GLint success_prog;
@@ -158,14 +161,16 @@ int main() {
 
     /* clang-format off */
     GLfloat vertices[] = {
-         // vertices        // colors
-         0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+         // vertices          // colors             // texture coordinates
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,     1.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,     1.0f, 0.0f,
     };
 
     GLuint indices[] = {
         0, 1, 2,
+        0, 2, 3,
     };
     /* clang-format on */
 
@@ -181,18 +186,88 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    /* bind the vbo to a slot on the vao */
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+    /* bind the vbo to a slot on the vao.
+     * as we keep adding more and more data to the same vbo,
+     * we need to update the stride for each attribute.
+     */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    /* set texture wrapping and filtering options */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    unsigned int TEXTURE0;  // maps to 0
+    unsigned int TEXTURE1;  // maps to 1
+
+    {
+        /* create a texture object */
+        glGenTextures(1, &TEXTURE0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, TEXTURE0);
+
+        /* load image into memory */
+        int width, height, nchannels;
+        stbi_set_flip_vertically_on_load(GL_TRUE);
+        unsigned char *data = stbi_load("assets/textures/container.jpg", &width, &height, &nchannels, 0);
+        if (!data) return EXIT_FAILURE;
+
+        /* bind texture data to the texture object */
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        /* free the image memory */
+        stbi_image_free(data);
+    }
+    {
+        /* create a texture object */
+        glGenTextures(1, &TEXTURE1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, TEXTURE1);
+
+        /* load image into memory */
+        int width, height, nchannels;
+        unsigned char *data = stbi_load("assets/textures/awesomeface.png", &width, &height, &nchannels, 0);
+        if (!data) return EXIT_FAILURE;
+
+        /* bind texture data to texture1 object */
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        /* free awesomeface.png */
+        stbi_image_free(data);
+    }
+    {
+        {
+            GLint TEXTURE0_loc = glGetUniformLocation(shader_program, "TEXTURE0");
+            if (TEXTURE0_loc == -1) {
+                ERROR("no uniform named TEXTURE0 found in shader_program\n");
+                return EXIT_FAILURE;
+            }
+            glUniform1i(TEXTURE0_loc, 0);
+        }
+        {
+            GLint TEXTURE1_loc = glGetUniformLocation(shader_program, "TEXTURE1");
+            if (TEXTURE1_loc == -1) {
+                ERROR("no uniform named TEXTURE1 found in shader_program\n");
+                return EXIT_FAILURE;
+            }
+            glUniform1i(TEXTURE1_loc, 1);
+        }
+    }
 
     while (!glfwWindowShouldClose(window)) {
         /* event */
         glfwPollEvents();
         process_input(window);
         /* event */
+
         /* renderer */
         glClear(GL_COLOR_BUFFER_BIT);
         glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, NULL);
